97529668c68f60df61ed2b0ad79323358095969d
Chaney <chaneyfonseca@gmail.com>
2024-05-15 18:58:55 -0500

    add tdee component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TdeeComponent } from './tdee/tdee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tdee', component: TdeeComponent }
+];
