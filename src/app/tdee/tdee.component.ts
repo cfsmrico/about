d702015070b6c0e26bbc786387c15dc880b8d22d
Chaney <chaneyfonseca@gmail.com>
2024-05-15 18:22:47 -0500

    add tdee component

---
+++ src/app/tdee/tdee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tdee',
+  standalone: true,
+  imports: [],
+  templateUrl: './tdee.component.html',
+  styleUrl: './tdee.component.css'
+})
+export class TdeeComponent {
+
+}
