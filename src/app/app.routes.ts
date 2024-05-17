53632d3a68f0bc03be0217b527e539aefb57c58b
Chaney <chaneyfonseca@gmail.com>
2024-05-17 18:01:32 -0500

    selectButtons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { TdeeComponent } from './tdee/tdee.component';
 
 export const routes: Routes = [
+  { path: '', component: TdeeComponent },
   { path: 'tdee', component: TdeeComponent }
 ];
--- src/app/tdee/tdee.component.ts
+++ src/app/tdee/tdee.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-tdee',
@@ -11,12 +13,16 @@ import { DropdownModule } from 'primeng/dropdown';
     RouterModule,
     FormsModule,
     CommonModule,
+    SelectButtonModule,
+    ButtonModule,
     DropdownModule,
   ],
   templateUrl: './tdee.component.html',
   styleUrl: './tdee.component.css'
 })
 export class TdeeComponent {
-  selectedCity: string = "";
-  cities: string[] = [];
+  unitSystemOptions = ['Metric', 'Imperial'];
+  unitSystem = 'Metric';
+  sexOptions = ['Male', 'Female'];
+  sex = 'Male';
 }
