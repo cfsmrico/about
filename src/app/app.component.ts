bf926bdac6df35e730a7010b998f407a1ebac5c0
Chaney <chaneyfonseca@gmail.com>
2024-05-24 17:45:45 -0500

    add menubar; add primeicons package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ButtonModule
+    ButtonModule,
+    MenubarModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cfsmrico.github.io';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'TDEE',
+        icon: 'pi pi-star'
+      }
+    ]
+  }
 }
