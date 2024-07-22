8433321d3ed0a95ae8c96b283a2c4f08b4ed5529
Chaney <chaneyfonseca@gmail.com>
2024-07-21 21:04:41 -0500

    sigh

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  output:'export',
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
@@ -25,7 +24,6 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
