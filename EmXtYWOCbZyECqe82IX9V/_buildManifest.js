ffdcd58ada844f46a2320f6d571726b9391ba0c3
Chaney <chaneyfonseca@gmail.com>
2024-07-21 20:59:39 -0500

    Updates

---
+++ EmXtYWOCbZyECqe82IX9V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/189-926772d4a8d222ed.js","static/chunks/pages/index-793ff73544f5c8fe.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
